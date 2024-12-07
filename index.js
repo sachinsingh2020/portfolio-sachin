7cc303c3d810de04b52f55d0b52e1a6f150e56f2
sachin singh <sachinthegreat1001@gmail.com>
2024-12-07 12:15:08 +0530

    my portfolio

---
+++ index.js
@@ -0,0 +1,18 @@
+var typed = new Typed('#typed', {
+    strings: ['Full Stack Developer', 'Web Designer', 'Graphic Designer', 'Web Developer', 'UI/UX Designer'],
+    stringselement: null,
+    typeSpeed: 20,
+    startDelay: 150,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop: true,
+    loopCount: 550,
+    showCursor: true,
+    cursorChar: '|',
+    attr: null,
+    contentTypeL: 'html',
+});
+
+// Mixitup 
+var container = $(".mixitup");
+mixitup(container);
